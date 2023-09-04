c0c6745e797ba7ee7a81524eeb388774a6e825fd
YourStepSis <100165808+YourStepSis999@users.noreply.github.com>
2023-09-04 15:31:37 -0400

    Add files via upload

--- Atelier3.go
+++ Atelier3.go
@@ -1,5 +1,15 @@
 package Atelier3
 
-func ArrayGenerate() string {
-	return "patata"
+import (
+	"math/rand"
+)
+
+// le lien du github du package est github.com/YourStepSis999/Atelier3
+
+func ArrayGenerate( nb int) []int{
+	array := []int{}
+	for i := 0; i < len(array); i++ {
+		array[i] = rand.Intn(10000 - 1) + 10000
+	}
+	return array
 }
